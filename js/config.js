0f110a5eb653ef02e7234c8b1f6bfb424810fd2c
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2020-11-05 22:32:34 -0300

    PORTIFOLIO TOTALMENTE MUDADO
    Mudanças de html,css,javascript,design tudo do zero

---
+++ js/config.js
@@ -0,0 +1,8 @@
+
+
+function acendelampada() {
+    document.getElementById("luz").src = arquivo = "imagem/lampada-acesa.png";
+}
+function apagalampada() { 
+    document.getElementById("luz").src = arquivo = "imagem/lampada-apagada.png";
+}

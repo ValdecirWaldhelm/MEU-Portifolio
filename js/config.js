fd3ebe5273168640628a53ce6612e34855aa438a
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2020-11-28 19:20:53 -0300

    mudanças no portifolio menu-hamburguer
    implementaçãp de menu hamburguer mais responsividade,acerto nos cli-path,melhorias em redes sociais,method post email,menu personalizado

--- js/config.js
+++ js/config.js
@@ -6,3 +6,31 @@ function acendelampada() {
 function apagalampada() { 
     document.getElementById("luz").src = arquivo = "imagem/lampada-apagada.png";
 }
+
+var iconMenu = document.querySelectorAll('.icon-menu');
+
+	iconMenu[0].addEventListener('click',()=>{
+			//Efeito de opacidade.
+			/*
+			let menu = document.getElementById('menu');
+			if(menu.classList.contains('hide')){
+				menu.classList.add('show');
+				menu.classList.remove('hide');
+			}else{
+				menu.classList.add('hide');
+				menu.classList.remove('show');
+			}
+			*/
+
+			//Efeito toggle
+			let menu = document.getElementById('menu');
+			if(menu.classList.contains('hide')){
+				menu.classList.add('show');
+				menu.classList.remove('hide');
+			}else{
+				menu.classList.add('hide');
+				menu.classList.remove('show');
+			}
+
+
+	});

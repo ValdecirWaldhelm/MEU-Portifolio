642765369df1413a9f96c5341b7c3fc73588e48b
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2021-02-11 22:55:58 -0300

    New Portfolio em construção

--- js/config.js
+++ js/config.js
@@ -7,30 +7,16 @@ function apagalampada() {
     document.getElementById("luz").src = arquivo = "imagem/lampada-apagada.png";
 }
 
-var iconMenu = document.querySelectorAll('.icon-menu');
 
-	iconMenu[0].addEventListener('click',()=>{
-			//Efeito de opacidade.
-			/*
-			let menu = document.getElementById('menu');
-			if(menu.classList.contains('hide')){
-				menu.classList.add('show');
-				menu.classList.remove('hide');
-			}else{
-				menu.classList.add('hide');
-				menu.classList.remove('show');
-			}
-			*/
+	$(function(){
 
-			//Efeito toggle
-			let menu = document.getElementById('menu');
-			if(menu.classList.contains('hide')){
-				menu.classList.add('show');
-				menu.classList.remove('hide');
-			}else{
-				menu.classList.add('hide');
-				menu.classList.remove('show');
-			}
+        // menu slide mobile
+        
+        $('.btn-menu').on('click touchstart', function () {
+            $('html').toggleClass('btn-menu-active');
+          });
 
 
-	});
+
+
+    });
